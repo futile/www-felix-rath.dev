98d397a83b2db48cb3c88772f3a9c2322a4ff7e4
Felix Rath <felixm.rath@gmail.com>
2024-02-22 22:11:11 +0100

    refactor!: start using maud! Pog!

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,21 @@
+use maud::{html, Markup, DOCTYPE};
 use worker::*;
+
+fn main_page() -> Markup {
+    html! {
+        (DOCTYPE)
+        head {
+            title { "Hello, Maud!" }
+        }
+        body {
+            h1 { "Hello, Maud-Title!" }
+            p { "Hello, World! 2" }
+            p { "Hello, World! 22" }
+        }
+    }
+}
 
 #[event(fetch)]
 async fn main(_req: Request, _env: Env, _ctx: Context) -> Result<Response> {
-    Response::from_html(
-        r#"
-    <html>
-        <head>
-            <title>Hello, World!</title>
-        </head>
-        <body>
-            <h1>Hello, World!</h1>
-            <p>Hello, World! 2</p>
-            <p>Hello, World! 12</p>
-        </body>
-    </html>
-    "#,
-    )
+    Response::from_html(main_page().into_string())
 }
